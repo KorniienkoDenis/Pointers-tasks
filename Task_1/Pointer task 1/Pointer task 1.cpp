98ffdc53b4660ee7362e493ff6f6824b61d0fec4
Denis Korniienko <kubikrubikmegaminks@gmail.com>
2023-03-15 14:12:50 +0300

    done: task_1 completed

--- Task_1/Pointer task 1/Pointer task 1.cpp
+++ Task_1/Pointer task 1/Pointer task 1.cpp
@@ -1,20 +1,81 @@
-// Pointer task 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <ctime>
+
+/*
+	
+	Task 1.
+		Declare a pointer on the array of type double and offer
+		the user choose it size. Next write four functions:
+			1. The first must allocate memory for the array;
+			2. The second - fill the array elements;
+			3. The third - show data in the console;
+			4. The fourth - to free memory;
+		A program must proposes the user continue the work
+		(created new dynamic arrays) or exit from the program.
+
+*/
+
+double* AllocateMemory(double* ptrArr, int sizePtr);
+void FillArray(double* ptrArr, int sizePtr);
+void ShowArray(double* ptrArr, int sizePtr);
+double* ToFreeMemory(double* ptrArr);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	srand(time(nullptr));
+
+	double* arr = 0;
+	char userAnswer;
+	
+	do
+	{
+		int sizeOfArray = 0;
+
+		std::cout << "Input size of the array: ";
+		std::cin >> sizeOfArray;
+
+		arr = AllocateMemory(arr, sizeOfArray);
+
+		FillArray(arr, sizeOfArray);
+		ShowArray(arr, sizeOfArray);
+
+		arr = ToFreeMemory(arr);
+
+		std::cout << std::endl;
+		std::cout << "Continue (1). Exit (0): ";
+		std::cin >> userAnswer;
+
+	} while (userAnswer != '0');
+
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+double* AllocateMemory(double* ptrArr, int sizePtr)
+{
+	ptrArr = new double[sizePtr];
+	return ptrArr;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void FillArray(double* ptrArr, int sizePtr)
+{
+	for (int index = 0; index < sizePtr; index++)
+		ptrArr[index] = rand() % 10 + 1;
+}
+
+void ShowArray(double* ptrArr, int sizePtr)
+{
+	std::cout << "Dynamic array: ";
+
+	for (int index = 0; index < sizePtr; index++)
+		std::cout << ptrArr[index] << " ";
+
+	std::cout << std::endl;
+}
+
+double* ToFreeMemory(double* ptrArr)
+{
+	delete[] ptrArr;
+	ptrArr = 0;
+
+	return ptrArr;
+}
